d83e5ce73252ddb1d5995580c2f02bb2ddf863e7
Namwoongchan <75806377+woongchan789@users.noreply.github.com>
2023-03-22 16:29:00 +0900

    Add files via upload

---
+++ pages/neural_style_transfer_model.py
@@ -0,0 +1,195 @@
+from __future__ import print_function
+from models.My_vgg19 import VGG19
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torch.nn as nn
+import torchvision.transforms as transforms
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+class ContentLoss(nn.Module):
+
+    def __init__(self, target,):
+        super(ContentLoss, self).__init__()
+        self.target = target.detach()
+
+    def forward(self, input):
+        self.loss = F.mse_loss(input, self.target)
+        return input
+
+class StyleLoss(nn.Module):
+
+    def __init__(self, target_feature):
+        super(StyleLoss, self).__init__()
+        self.target = gram_matrix(target_feature).detach()
+
+    def forward(self, input):
+        G = gram_matrix(input)
+        self.loss = F.mse_loss(G, self.target)
+        return input
+
+def gram_matrix(input):
+    a, b, c, d = input.size()  
+    features = input.view(a * b, c * d)  
+    G = torch.mm(features, features.t())
+    return G.div(a * b * c * d)
+
+cnn = VGG19().features.to(device).eval()
+
+class Normalization(nn.Module):
+    def __init__(self, mean, std):
+        super(Normalization, self).__init__()
+        self.mean = torch.tensor(mean).view(-1, 1, 1)
+        self.std = torch.tensor(std).view(-1, 1, 1)
+
+    def forward(self, img):
+        return (img - self.mean) / self.std
+    
+# Style / Content 손실 계산을 원하는 계층
+content_layers_default = ['conv_4']
+style_layers_default = ['conv_1', 'conv_2', 'conv_3', 'conv_4', 'conv_5']
+
+def get_style_model_and_losses(cnn, style_img, content_img,
+                               content_layers=content_layers_default,
+                               style_layers=style_layers_default):
+    
+    # image transform
+    loader = transforms.Compose([
+    transforms.Resize(content_img.size),  
+    transforms.ToTensor()]) 
+
+    def image_loader(image):
+        scaled_image = loader(image).unsqueeze(0)
+        return scaled_image.to(device, torch.float)
+
+    style_img = image_loader(style_img)
+    content_img = image_loader(content_img)
+
+    normalization_mean = torch.tensor([0.485, 0.456, 0.406]).to(device)
+    normalization_std = torch.tensor([0.229, 0.224, 0.225]).to(device)
+
+    normalization = Normalization(normalization_mean, normalization_std).to(device)
+
+    # content, style loss calculate
+    content_losses = []
+    style_losses = []
+
+    model = nn.Sequential(normalization)
+
+    i = 0  
+    for layer in cnn.children():
+        if isinstance(layer, nn.Conv2d):
+            i += 1
+            name = 'conv_{}'.format(i)
+        elif isinstance(layer, nn.ReLU):
+            name = 'relu_{}'.format(i)
+            layer = nn.ReLU(inplace=False)
+        elif isinstance(layer, nn.MaxPool2d):
+            name = 'pool_{}'.format(i)
+        elif isinstance(layer, nn.BatchNorm2d):
+            name = 'bn_{}'.format(i)
+        else:
+            raise RuntimeError('Unrecognized layer: {}'.format(layer.__class__.__name__))
+
+        model.add_module(name, layer)
+
+        if name in content_layers:
+            # Content 손실 추가
+            target = model(content_img).detach()
+            content_loss = ContentLoss(target)
+            model.add_module("content_loss_{}".format(i), content_loss)
+            content_losses.append(content_loss)
+
+        if name in style_layers:
+            # Style 손실 추가
+            target_feature = model(style_img).detach()
+            style_loss = StyleLoss(target_feature)
+            model.add_module("style_loss_{}".format(i), style_loss)
+            style_losses.append(style_loss)
+
+    for i in range(len(model) - 1, -1, -1):
+        if isinstance(model[i], ContentLoss) or isinstance(model[i], StyleLoss):
+            break
+
+    model = model[:(i + 1)]
+
+    return model, style_losses, content_losses
+
+def get_input_optimizer(input_img):
+    # 입력이 기울기가 필요한 매개 변수임을 표시하는 줄
+    optimizer = optim.LBFGS([input_img])
+    return optimizer
+
+def run_style_transfer(cnn, content_img, style_img, num_steps=150,
+                       style_weight=80000, content_weight=1):
+    print('Building the style transfer model..')
+    model, style_losses, content_losses = get_style_model_and_losses(cnn, style_img, content_img)
+
+    width, height = content_img.width, content_img.height
+    
+    loader = transforms.Compose([
+    transforms.Resize(content_img.size),  
+    transforms.ToTensor()]) 
+
+    def image_loader(image):
+        scaled_image = loader(image).unsqueeze(0)
+        return scaled_image.to(device, torch.float)
+
+    content_img = image_loader(content_img)
+
+    # 초기 이미지 생성
+    input_img = content_img.clone()
+
+    # 모델의 매개변수를 제외한 입력을 최적화해야 하므로
+    # 이에 맞춰서 requires_grad 값을 갱신합니다.
+    input_img.requires_grad_(True)
+    model.requires_grad_(False)
+
+    optimizer = get_input_optimizer(input_img)
+
+    print('Optimizing..')
+    run = [0]
+    while run[0] <= num_steps:
+
+        def closure():
+            # 업데이트 된 입력 이미지의 값을 수정
+            with torch.no_grad():
+                input_img.clamp_(0, 1)
+
+            optimizer.zero_grad()
+            model(input_img)
+            style_score = 0
+            content_score = 0
+
+            for sl in style_losses:
+                style_score += sl.loss
+            for cl in content_losses:
+                content_score += cl.loss
+
+            style_score *= style_weight
+            content_score *= content_weight
+
+            loss = style_score + content_score
+            loss.backward()
+
+            run[0] += 1
+            if run[0] % 50 == 0:
+                print("run {}:".format(run))
+                print('Style Loss : {:4f} Content Loss: {:4f}'.format(
+                    style_score.item(), content_score.item()))
+                print()
+
+            return style_score + content_score
+
+        optimizer.step(closure)
+
+    with torch.no_grad():
+        input_img.clamp_(0, 1)
+        transform = transforms.ToPILImage()
+        img = transform(input_img.squeeze(0))
+        final_img = img.resize((width, height))
+        return final_img
+        
