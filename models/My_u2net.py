a0cb52bd355e521dbfd4cc6b2365151e2f434954
Namwoongchan <75806377+woongchan789@users.noreply.github.com>
2023-03-24 17:38:09 +0900

    Update My_u2net.py

--- models/My_u2net.py
+++ models/My_u2net.py
@@ -1,6 +1,6 @@
 import io
 from enum import Enum
-from typing import Dict, List, Tuple, Union, Type
+from typing import Type
 import onnxruntime as ort
 import numpy as np
 from cv2 import (
@@ -13,59 +13,14 @@ from cv2 import (
 )
 from PIL import Image
 from PIL.Image import Image as PILImage
-from pymatting.alpha.estimate_alpha_cf import estimate_alpha_cf
-from pymatting.foreground.estimate_foreground_ml import estimate_foreground_ml
-from pymatting.util.util import stack_images
-from scipy.ndimage import binary_erosion
 import pooch
 import os
 from pathlib import Path
-
-kernel = getStructuringElement(MORPH_ELLIPSE, (3, 3))
-
 
 class ReturnType(Enum):
     BYTES = 0
     PILLOW = 1
     NDARRAY = 2
-
-
-def alpha_matting_cutout(img, mask, foreground_threshold, background_threshold, erode_structure_size):
-    
-    if img.mode == "RGBA" or img.mode == "CMYK":
-        img = img.convert("RGB")
-
-    img = np.asarray(img)
-    mask = np.asarray(mask)
-
-    is_foreground = mask > foreground_threshold
-    is_background = mask < background_threshold
-
-    structure = None
-    if erode_structure_size > 0:
-        structure = np.ones(
-            (erode_structure_size, erode_structure_size), dtype=np.uint8
-        )
-
-    is_foreground = binary_erosion(is_foreground, structure=structure)
-    is_background = binary_erosion(is_background, structure=structure, border_value=1)
-
-    trimap = np.full(mask.shape, dtype=np.uint8, fill_value=128)
-    trimap[is_foreground] = 255
-    trimap[is_background] = 0
-
-    img_normalized = img / 255.0
-    trimap_normalized = trimap / 255.0
-
-    alpha = estimate_alpha_cf(img_normalized, trimap_normalized)
-    foreground = estimate_foreground_ml(img_normalized, alpha)
-    cutout = stack_images(foreground, alpha)
-
-    cutout = np.clip(cutout * 255, 0, 255).astype(np.uint8)
-    cutout = Image.fromarray(cutout)
-
-    return cutout
-
 
 def naive_cutout(img, mask):
     empty = Image.new("RGBA", (img.size), 0)
@@ -88,15 +43,13 @@ def get_concat_v(img1, img2):
 
 
 def post_process(mask):
-    mask = morphologyEx(mask, MORPH_OPEN, kernel)
+    mask = morphologyEx(mask, MORPH_OPEN, getStructuringElement(MORPH_ELLIPSE, (3, 3)))
     mask = GaussianBlur(mask, (5, 5), sigmaX=2, sigmaY=2, borderType=BORDER_DEFAULT)
     mask = np.where(mask < 127, 0, 255).astype(np.uint8)
     return mask
 
 
-def remove(data, alpha_matting=False, alpha_matting_foreground_threshold=240,
-           alpha_matting_background_threshold=10, alpha_matting_erode_size=10,
-           only_mask=False, post_process_mask=False) :
+def remove(data, post_process_mask=False) :
 
     if isinstance(data, PILImage):
         return_type = ReturnType.PILLOW
@@ -118,25 +71,7 @@ def remove(data, alpha_matting=False, alpha_matting_foreground_threshold=240,
     for mask in masks:
         if post_process_mask:
             mask = Image.fromarray(post_process(np.array(mask)))
-
-        if only_mask:
-            cutout = mask
-
-        elif alpha_matting:
-            try:
-                cutout = alpha_matting_cutout(
-                    img,
-                    mask,
-                    alpha_matting_foreground_threshold,
-                    alpha_matting_background_threshold,
-                    alpha_matting_erode_size,
-                )
-            except ValueError:
-                cutout = naive_cutout(img, mask)
-
-        else:
-            cutout = naive_cutout(img, mask)
-
+        cutout = naive_cutout(img, mask)
         cutouts.append(cutout)
 
     cutout = img
